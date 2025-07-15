3b08de3256a2dab141e1cdf5fa249bb4728743ff
czxieddan <czxieddan@gmail.com>
2025-07-16 03:39:25 +0800

    xia ban louuuu!!!
    upload some goals icons
    remove the nukes from topbar.gui
    add the ec. and interchange internationalmarket
    add some localisation for the PRC_00 and PRC_01 from Writing group

--- common/defines/01_career_profile.lua
+++ common/defines/01_career_profile.lua
@@ -2,8 +2,8 @@ NDefines_CareerProfile = {
 
 NCareerProfile = {
 	-- Mod defines
-	MOD_STATISTICS_GROUP = "", -- Can be set by mods to collect statistics in a separate data set for the mod. Will also be used as the display name if MOD_STATISTICS_GROUP_NAME is not set.
-	MOD_STATISTICS_GROUP_NAME = "", -- Can be set by mods as a display name for the mod's statistics data set. It can be a localized.
+	MOD_STATISTICS_GROUP = "ASPF", -- Can be set by mods to collect statistics in a separate data set for the mod. Will also be used as the display name if MOD_STATISTICS_GROUP_NAME is not set.
+	MOD_STATISTICS_GROUP_NAME = "ASPF", -- Can be set by mods as a display name for the mod's statistics data set. It can be a localized.
 
 	-- Statistics parameters
 	NAVAL_INVASION_MEDAL_MAX_DURATION = 96, -- Maximum duration in hours to finish naval invasion and get a Naval Invasion Medal
